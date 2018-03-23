28e174543acea462367cec13d1c22129ee0232ef
Colt Bauman <cabauman@maverickgames.co>
2018-03-23 22:50:25 +0900

    Add tests for offline and HttpClient calls

---
+++ src/Firebase.Tests/FirebaseHttpClientTests.cs
@@ -0,0 +1,205 @@
+﻿namespace Firebase.Database.Tests
+{
+    using Firebase.Database;
+    using Firebase.Database.Query;
+    using Firebase.Database.Streaming;
+    using Firebase.Database.Tests.Entities;
+    using Firebase.Database.Tests.Utils;
+    using FluentAssertions;
+    using Microsoft.Reactive.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class FirebaseHttpClientTests
+    {
+        public const string BasePath = "http://base.path.net";
+
+        const string ResponseData_OneDino = @"{
+              ""dino1"": {
+                ""ds"": {
+                  ""height"" : 1,
+                  ""length"" : 1,
+                  ""weight"": 1
+                }
+              }
+            }";
+
+        const string ResponseData_Dino3Height = @"{
+              ""dino3"": {
+                ""ds"": {
+                  ""height"" : 1
+                }
+              }
+            }";
+
+        const string ResponseData_TwoDinos = @"{
+              ""dino2"": {
+                ""ds"": {
+                  ""height"" : 2,
+                  ""length"" : 2,
+                  ""weight"": 2
+                }
+              },
+              ""dino3"": {
+                ""ds"": {
+                  ""height"" : 3,
+                  ""length"" : 3,
+                  ""weight"": 3
+                }
+              }
+            }";
+
+        [TestMethod]
+        public void PostAsyncShouldNotThrowAnException()
+        {
+            var client = GetFirebaseClient(ResponseData_OneDino);
+
+            Func<Task> post = async () => { await client.Child("dinos").PostAsync(ResponseData_OneDino); };
+            post.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void PutAsyncShouldNotThrowAnException()
+        {
+            var client = GetFirebaseClient(ResponseData_OneDino);
+
+            Func<Task> put = async () => { await client.Child("dinos/dino1").PutAsync(ResponseData_OneDino); };
+            put.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void PutAsyncShouldThrowAnExceptionBecauseTheRequestUriIsWrong()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/WRONG_PATH/.json?print=silent");
+            var client = GetFirebaseClient(ResponseData_OneDino, requestUri);
+
+            Func<Task> put = async () => { await client.Child("dinos/dino1").PutAsync(ResponseData_OneDino); };
+            put.ShouldThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void PatchAsyncShouldNotThrowAnException()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino3/.json?print=silent");
+            var client = GetFirebaseClient(ResponseData_Dino3Height, requestUri);
+
+            Func<Task> patch = async () => { await client.Child("dinos/dino3").PatchAsync(ResponseData_Dino3Height); };
+            patch.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void PatchAsyncShouldThrowAnExceptionBecauseTheChildPathIsWrong()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino3/.json");
+            var client = GetFirebaseClient(ResponseData_Dino3Height, requestUri);
+
+            Func<Task> patch = async () => { await client.Child("WRONG_PATH").PatchAsync(ResponseData_Dino3Height); };
+            patch.ShouldThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void OnceSingleAsyncShouldNotThrowAnException()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino1/.json");
+            var client = GetFirebaseClient(ResponseData_OneDino, requestUri);
+
+            Func<Task> onceSingle = async () => { await client.Child("dinos/dino1").OnceSingleAsync<Dinosaur>(); };
+            onceSingle.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void OnceSingleAsyncShouldThrowAnExceptionBecauseChildPathIsWrong()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino1/.json");
+            var client = GetFirebaseClient(ResponseData_OneDino, requestUri);
+
+            Func<Task> onceSingle = async () => { await client.Child("WRONG_PATH").OnceSingleAsync<Dinosaur>(); };
+            onceSingle.ShouldThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void OnceAsyncShouldNotThrowAnException()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/.json");
+            var client = GetFirebaseClient(ResponseData_TwoDinos, requestUri);
+
+            Func<Task> once = async () => { await client.Child("dinos").OnceAsync<Dinosaur>(); };
+            once.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void OnceAsyncShouldThrowAnExceptionBecauseTheChildPathIsWrong()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/.json");
+            var client = GetFirebaseClient(ResponseData_TwoDinos, requestUri);
+
+            Func<Task> once = async () => { await client.Child("WRONG_PATH").OnceAsync<Dinosaur>(); };
+            once.ShouldThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void DeleteAsyncShouldNotThrowAnException()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino1/.json");
+            var client = GetFirebaseClient(ResponseData_OneDino, requestUri);
+
+            Func<Task> delete = async () => { await client.Child("dinos/dino1").DeleteAsync(); };
+            delete.ShouldNotThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void DeleteAsyncShouldThrowAnExceptionBecauseTheChildPathIsWrong()
+        {
+            var requestUri = new Uri(BasePath + "/dinos/dino1/.json");
+            var client = GetFirebaseClient(ResponseData_OneDino, requestUri);
+
+            Func<Task> delete = async () => { await client.Child("WRONG_PATH").DeleteAsync(); };
+            delete.ShouldThrow<FirebaseException>();
+        }
+
+        [TestMethod]
+        public void RetrieveAStreamOfTwoDinosaursUsingOnceAsyncAsAnObservable()
+        {
+            var client = GetFirebaseClient(ResponseData_TwoDinos);
+            var stream = client.Child("dinos")
+                .OnceAsync<Dinosaur>()
+                .ToObservable()
+                .SelectMany(x => x)
+                .Select(x => x.Key);
+
+            TestScheduler scheduler = new TestScheduler();
+            ITestableObserver<string> results = scheduler.CreateObserver<string>();
+            stream.Subscribe(results);
+            scheduler.Start();
+
+            results.Messages.AssertEqual(
+                ReactiveTest.OnNext(0, "dino2"),
+                ReactiveTest.OnNext(0, "dino3"),
+                ReactiveTest.OnCompleted<string>(0));
+        }
+
+        private FirebaseClient GetFirebaseClient(string responseData, Uri requestUri = null)
+        {
+            var messageResponse = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData);
+            var httpMsgOptions = new HttpMessageOptions
+            {
+                RequestUri = requestUri,
+                HttpResponseMessage = messageResponse
+            };
+
+            FirebaseOptions firebaseOptions = new FirebaseOptions()
+            {
+                HttpMessageHandler = new FakeHttpMessageHandler(httpMsgOptions)
+            };
+
+            var client = new FirebaseClient(BasePath, firebaseOptions);
+
+            return client;
+        }
+    }
+}
--- src/Firebase.Tests/OfflineTests.cs
+++ src/Firebase.Tests/OfflineTests.cs
@@ -1,148 +1,255 @@
 ﻿namespace Firebase.Database.Tests
 {
     using Firebase.Database;
-    using Firebase.Database.Tests.Entities;
     using Firebase.Database.Offline;
+    using Firebase.Database.Streaming;
+    using Firebase.Database.Tests.Entities;
+    using Firebase.Database.Tests.Utils;
     using FluentAssertions;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Microsoft.Reactive.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
+    using System.Collections.Generic;
     using System.Linq;
-    using System.Reactive;
     using System.Reactive.Concurrency;
-    using System.Reactive.Disposables;
     using System.Reactive.Linq;
-    using System.Threading.Tasks;
-    using WorldDomination.Net.Http;
-    using Firebase.Database.Query;
 
     [TestClass]
     public class OfflineTests
     {
         public const string BasePath = "http://base.path.net";
 
-        [TestMethod]
-        public void Test()
-        {
-            FirebaseOptions options = new FirebaseOptions()
-            {
-                OfflineDatabaseFactory = (t, s) =>
-                {
-                    return new OfflineDatabase(t, s);
-                }
-            };
-            
-            var client = new FirebaseClient(BasePath, options);
-
-            var dinosaurDB = client.Child("dinos").AsRealtimeDatabase<Dinosaur>(
-                "",
-                "",
-                StreamingOptions.None,
-                InitialPullStrategy.None,
-                true);
-
-            dinosaurDB.Database.Clear();
-            Dinosaur dino = new Dinosaur(5, 20, 30);
-            dinosaurDB.Post(dino);
-
-            //var dinosaurDB2 = client.Child("dinos").AsRealtimeDatabase<Dinosaur>(
-            //    "",
-            //    "",
-            //    StreamingOptions.LatestOnly,
-            //    InitialPullStrategy.MissingOnly,
-            //    true);
-
-            //Assert.AreEqual(dinosaurDB2.Database.Count, 1);
-
-            TestScheduler scheduler = new TestScheduler();
-
-            var dino1 = new Dinosaur(1, 1, 1);
-            scheduler.Schedule(TimeSpan.FromTicks(10), () =>
-            {
-                dinosaurDB.Post(dino1);
-            });
-
-            //ITestableObserver<Dinosaur> results = scheduler.Start(
-            //    () => dinosaurDB.AsObservable().Select(x => x.Object),
-            //    created: 0,
-            //    subscribed: 5,
-            //    disposed: 100);
-            ITestableObserver<Dinosaur> results = scheduler.CreateObserver<Dinosaur>();
-            dinosaurDB.AsObservable().Select(x => x.Object).Subscribe(results);
-            scheduler.Start();
-            results.Messages.AssertEqual(
-                ReactiveTest.OnNext(0, dino),
-                ReactiveTest.OnNext(10, dino1));
-            
-            //var dino2 = new Dinosaur(2, 2, 2);
-            //var dino3 = new Dinosaur(3, 3, 3);
-
-            //ITestableObservable<Dinosaur> source = scheduler.CreateHotObservable(
-            //    ReactiveTest.OnNext(10, dino1),
-            //    ReactiveTest.OnNext(20, dino2),
-            //    ReactiveTest.OnNext(30, dino3));
-
-            //ITestableObserver<Dinosaur> results = scheduler.CreateObserver<Dinosaur>();
-            //source.Subscribe(results);
-            //scheduler.Start();
-            //results.Messages.AssertEqual(
-            //    ReactiveTest.OnNext(10, dino1),
-            //    ReactiveTest.OnNext(20, dino2),
-            //    ReactiveTest.OnNext(30, dino3));
-
-            //Assert.AreEqual(dinosaurDB.Database.Count, 3);
-
-            dinosaurDB.Database.Clear();
-        }
-
-        [TestMethod]
-        public async Task PushDataAsync()
-        {
-            const string responseData = @"{
+        const string ResponseData_OneDino = @"{
               ""dino1"": {
+                ""ds"": {
+                  ""height"" : 1,
+                  ""length"" : 1,
+                  ""weight"": 1
+                }
+              }
+            }";
+
+        const string ResponseData_TwoDinos = @"{
+              ""dino2"": {
                 ""ds"": {
                   ""height"" : 2,
                   ""length"" : 2,
                   ""weight"": 2
                 }
-              }";
+              },
+              ""dino3"": {
+                ""ds"": {
+                  ""height"" : 3,
+                  ""length"" : 3,
+                  ""weight"": 3
+                }
+              }
+            }";
 
-            //const string responseData = "{ \"Id\":69, \"Name\":\"Jane\" }";
+        [TestMethod]
+        public void PostDinosaurAndMakeSureItsLoadedWhenCreatingANewDbRef()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB();
+            RealtimeDatabase<Dinosaur> dinosaurDB2 = null;
+
+            try
+            {
+                Dinosaur dino = new Dinosaur(5, 20, 30);
+                dinosaurDB.Post(dino);
+
+                dinosaurDB2 = GetOfflineDinosaurDB();
+
+                Assert.AreEqual(1, dinosaurDB.Database.Count);
+                Assert.AreEqual(1, dinosaurDB2.Database.Count);
+            }
+            finally
+            {
+                CleanUp(dinosaurDB, dinosaurDB2);
+            }
+        }
+
+        [TestMethod]
+        public void PutDinosaurAndMakeSureItsIdenticalToTheOneInTheDatabase()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB();
+
+            try
+            {
+                Dinosaur dino = new Dinosaur(5, 20, 30);
+                dinosaurDB.Put("dino1", dino);
+
+                var recoveredDino = dinosaurDB.Database["dino1"].Deserialize<Dinosaur>();
+
+                Assert.AreEqual(dino.Dimensions.Height, recoveredDino?.Dimensions.Height);
+                Assert.AreEqual(dino.Dimensions.Length, recoveredDino?.Dimensions.Length);
+                Assert.AreEqual(dino.Dimensions.Weight, recoveredDino?.Dimensions.Weight);
+            }
+            finally
+            {
+                CleanUp(dinosaurDB);
+            }
+        }
+
+        [TestMethod]
+        public void PostThreeDinosaursAndRetrieveThemAll()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB();
+
+            try
+            {
+                Dinosaur dino1 = new Dinosaur(1, 1, 1);
+                dinosaurDB.Post(dino1);
+                Dinosaur dino2 = new Dinosaur(2, 2, 2);
+                dinosaurDB.Post(dino2);
+                Dinosaur dino3 = new Dinosaur(3, 3, 3);
+                dinosaurDB.Post(dino3);
+
+                IEnumerable<FirebaseObject<Dinosaur>> offlineDinos = dinosaurDB.Once();
+
+                Assert.AreEqual(3, offlineDinos.Count());
+            }
+            finally
+            {
+                CleanUp(dinosaurDB);
+            }
+        }
+
+        // TODO: Figure out why this test passes when run alone, but fails when running as a group.
+        [TestMethod]
+        public void ObserveDbAndDoInitialPullOfEverything()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB(ResponseData_TwoDinos, StreamingOptions.None, InitialPullStrategy.Everything);
+
+            try
+            {
+                var stream = dinosaurDB
+                    .AsObservable()
+                    .Where(x => x.Object != null);
+
+                TestScheduler scheduler = new TestScheduler();
+                ITestableObserver<FirebaseEvent<Dinosaur>> results = scheduler.Start(
+                    () => stream,
+                    created: 0,
+                    subscribed: 0,
+                    disposed: 10);
+
+                Assert.AreEqual(2, results.Messages.Count);
+                Assert.AreEqual(2, dinosaurDB.Database.Count());
+
+                var firebaseEvent1 = results.Messages[0].Value.Value;
+                firebaseEvent1.Key.ShouldBeEquivalentTo("dino2");
+                firebaseEvent1.EventSource.ShouldBeEquivalentTo(FirebaseEventSource.OnlineInitial);
+                firebaseEvent1.EventType.ShouldBeEquivalentTo(FirebaseEventType.InsertOrUpdate);
+            }
+            finally
+            {
+                CleanUp(dinosaurDB);
+            }
+        }
+
+        [TestMethod]
+        public void ObserveDbAndMakeSureNoEventsAreReceived()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB(ResponseData_TwoDinos, StreamingOptions.None, InitialPullStrategy.None);
+
+            try
+            {
+                var stream = dinosaurDB.AsObservable()
+                    .Where(x => x.Object != null);
+
+                TestScheduler scheduler = new TestScheduler();
+                ITestableObserver<FirebaseEvent<Dinosaur>> results = scheduler.CreateObserver<FirebaseEvent<Dinosaur>>();
+                stream.Subscribe(results);
+                scheduler.Start();
+
+                Assert.AreEqual(0, results.Messages.Count);
+                Assert.AreEqual(0, dinosaurDB.Database.Count());
+            }
+            finally
+            {
+                CleanUp(dinosaurDB);
+            }
+        }
+
+        [TestMethod]
+        public void ObserveDbAndMakeSureTwoPostedDinosaursAreReceived()
+        {
+            var dinosaurDB = GetOfflineDinosaurDB(ResponseData_TwoDinos, StreamingOptions.None, InitialPullStrategy.None);
+
+            try
+            {
+                var stream = dinosaurDB.AsObservable()
+                    .Where(x => x.Object != null);
+
+                TestScheduler scheduler = new TestScheduler();
+                scheduler.Schedule(TimeSpan.FromTicks(1), () =>
+                {
+                    dinosaurDB.Post(new Dinosaur(4, 4, 4));
+                });
+                scheduler.Schedule(TimeSpan.FromTicks(5), () =>
+                {
+                    dinosaurDB.Post(new Dinosaur(5, 5, 5));
+                });
+
+                ITestableObserver<FirebaseEvent<Dinosaur>> results = scheduler.CreateObserver<FirebaseEvent<Dinosaur>>();
+                stream.Subscribe(results);
+                scheduler.Start();
+
+                Assert.AreEqual(2, results.Messages.Count);
+                Assert.AreEqual(2, dinosaurDB.Database.Count());
+
+                var firebaseEvent1 = results.Messages[0].Value.Value;
+                firebaseEvent1.EventSource.ShouldBeEquivalentTo(FirebaseEventSource.Offline);
+                firebaseEvent1.EventType.ShouldBeEquivalentTo(FirebaseEventType.InsertOrUpdate);
+                results.Messages[1].Time.ShouldBeEquivalentTo(5);
+            }
+            finally
+            {
+                CleanUp(dinosaurDB);
+            }
+        }
+
+        private RealtimeDatabase<Dinosaur> GetOfflineDinosaurDB(
+            string responseData = "",
+            StreamingOptions streamingOptions = StreamingOptions.LatestOnly,
+            InitialPullStrategy initialPullStrategy = InitialPullStrategy.MissingOnly)
+        {
             var messageResponse = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData);
-
-            // Prepare our 'options' with all of the above fake stuff.
             var httpMsgOptions = new HttpMessageOptions
             {
-                //RequestUri = new Uri(BasePath + "/dinos/dino1"),
                 HttpResponseMessage = messageResponse
             };
 
-
             FirebaseOptions firebaseOptions = new FirebaseOptions()
             {
-                OfflineDatabaseFactory = (t, s) =>
-                {
-                    return new OfflineDatabase(t, s);
-                }
+                OfflineDatabaseFactory = (t, s) => new OfflineDatabase(t, s),
+                HttpMessageHandler = new FakeHttpMessageHandler(httpMsgOptions)
             };
 
-            var httpMessageHandler = new FakeHttpMessageHandler(httpMsgOptions);
-            var client = new FirebaseClient(BasePath, firebaseOptions, httpMessageHandler);
+            var client = new FirebaseClient(BasePath, firebaseOptions);
 
             var dinosaurDB = client.Child("dinos").AsRealtimeDatabase<Dinosaur>(
-                "",
-                "",
-                StreamingOptions.Everything,
-                InitialPullStrategy.None,
+                string.Empty,
+                string.Empty,
+                streamingOptions,
+                initialPullStrategy,
                 true);
 
-            TestScheduler scheduler = new TestScheduler();
-            ITestableObserver<Dinosaur> results = scheduler.CreateObserver<Dinosaur>();
-            dinosaurDB.AsObservable().Select(x => x.Object).Subscribe(results);
+            return dinosaurDB;
+        }
 
-            await client.Child("dinos/dino1").PutAsync(responseData);
+        private void CleanUp<T>(params RealtimeDatabase<T>[] databases)
+            where T : class
+        {
+            foreach(var db in databases)
+            {
+                (db?.Database as OfflineDatabase)?.ReleaseFile();
+            }
 
-            results.Messages.Count.ShouldBeEquivalentTo(1);
+            foreach(var db in databases)
+            {
+                (db?.Database as OfflineDatabase)?.DeleteFile();
+            }
         }
     }
 }
